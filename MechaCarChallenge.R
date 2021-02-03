fc4a34781f64ff2cdef649073125d89b513925c1
jcramem <72235271+jcramem@users.noreply.github.com>
2021-02-03 15:14:20 -0600

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -25,3 +25,22 @@ lot_summary <- sc %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),
                                                                 Variance=var(PSI),
                                                                 SD=sd(PSI), .groups = 'keep')
 
+
+#t-test PSI all manufacturing lots against mean PSI of the population
+t.test(sc$PSI,mu = 1500)
+# t.test((sc$PSI),mu=mean((sc$PSI))) 
+
+#t-test PSI lot 1 against mean PSI of the population
+ls_1 <- sc[sc$Manufacturing_Lot == "Lot1",]
+t.test(ls_1$PSI,mu = 1500)
+# t.test((ls_1$PSI),mu=mean((sc$PSI))) 
+
+#t-test PSI lot 2 against mean PSI of the population
+ls_2 <- sc[sc$Manufacturing_Lot == "Lot2",]
+t.test(ls_2$PSI,mu = 1500)
+# t.test((ls_2$PSI),mu=mean((sc$PSI))) 
+
+#t-test PSI lot 3 against mean PSI of the population
+ls_3 <- sc[sc$Manufacturing_Lot == "Lot3",]
+t.test(ls_3$PSI,mu = 1500)
+# t.test((ls_3$PSI),mu=mean((sc$PSI))) 

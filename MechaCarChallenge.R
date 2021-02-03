9949d08454780df43378561109ea21207562fdda
jcramem <72235271+jcramem@users.noreply.github.com>
2021-02-03 10:11:22 -0600

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -9,3 +9,19 @@ head(mcm)
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mcm) 
 #generate summary statistics
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mcm)) 
+
+#import suspension coil data set
+sc <- read.csv('Suspension_Coil.csv',check.names = F,stringsAsFactors = F) 
+
+#create total summary
+total_summary <- sc %>% summarize(Mean=mean(PSI),
+                                  Median=median(PSI),
+                                  Variance=var(PSI),
+                                  SD=sd(PSI), .groups = 'keep')
+
+#create lot summary
+lot_summary <- sc %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),
+                                                                Median=median(PSI),
+                                                                Variance=var(PSI),
+                                                                SD=sd(PSI), .groups = 'keep')
+
